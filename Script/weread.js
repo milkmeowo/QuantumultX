505c3a68289881c267abadd0c62bec2b197ea62f
milkmeowo <milkmeowo@gmail.com>
2020-07-21 21:23:52 +0800

    Update weread.js

--- Script/weread.js
+++ Script/weread.js
@@ -3,6 +3,7 @@ const path1 = "/pay/memberCardSummary";
 const path2 = "/pay/balance";
 var path = $request.path;
 console.log('req-path',path)
+console.log('req',$request)
 function modify_card() {
   let obj = JSON.parse(body);
   let day = 1000*86400
